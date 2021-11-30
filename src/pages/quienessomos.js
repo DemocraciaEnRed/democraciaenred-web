7fa5b618ae7880462ffa001b0d9f8d6792586ce1
lieta96 <julieta.garcia96@gmai.com>
2021-11-30 14:54:40 -0300

    nueuva seccion nuestras memorias

--- src/pages/quienessomos.js
+++ src/pages/quienessomos.js
@@ -10,6 +10,7 @@ import TeamSlider from "../components/team-slider"
 import teamData from "../../content/team.json"
 import WorkWith from "../components/work-with-section"
 import dataUs from "../../content/us.json"
+import Memories from "../components/memories"
 
 const QuienesSomos = () => {
 
@@ -19,6 +20,7 @@ const QuienesSomos = () => {
        <Navbar/>
        <UsHeader data={dataUs}/>
        <Mision data={dataUs}/>
+       <Memories data={dataUs}/>
        <TeamSlider slides={teamData} title={dataUs}/>
        <WorkWith/>
        <Footer/> 
