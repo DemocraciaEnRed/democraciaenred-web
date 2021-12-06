c4635ce2eb86672ebc44efa78faf33d0dfd8346b
lieta96 <julieta.garcia96@gmai.com>
2021-12-06 11:37:53 -0300

    actualizacion del equipo

--- src/components/internal-page-hero/index.js
+++ src/components/internal-page-hero/index.js
@@ -1,17 +1,23 @@
-import React from "react"
-import { useIntl } from "gatsby-plugin-intl"
+import React from "react";
+import { useIntl } from "gatsby-plugin-intl";
 
-const InternalPageHero = ({ data }) =>  {
-    const intl = useIntl()
-    return(
-        <section className="hero is-medium has-background-black" >
-            <div className="hero-body has-text-centered is-flex-mobile is-justify-content-center is-flex-direction-column">
-                <h1 className="is-spaced has-text-yellow mb-5" >{intl.formatMessage({ id: data.title })}</h1>
-                <p className="mx-auto subtitle has-text-white">
-                    {intl.formatMessage({ id: data.description })}
-                </p>
-            </div>
-        </section>
-    )
-}
-export default InternalPageHero 
+const InternalPageHero = ({ data }) => {
+  const intl = useIntl();
+  return (
+    <section className="hero is-medium has-background-black">
+      <div className="hero-body has-text-centered ">
+        <div className="columns is-justify-content-center">
+          <div className="column is-9">
+            <h1 className="is-spaced has-text-yellow mb-5">
+              {intl.formatMessage({ id: data.title })}
+            </h1>
+            <p className="mx-auto subtitle has-text-white">
+              {intl.formatMessage({ id: data.description })}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default InternalPageHero;
--- src/components/us-header/index.js
+++ src/components/us-header/index.js
@@ -1,19 +1,21 @@
-import React from "react"
-import { useIntl } from "gatsby-plugin-intl"
-import './style.scss'
+import React from "react";
+import { useIntl } from "gatsby-plugin-intl";
+import "./style.scss";
 
-const UsHeader = ({ data }) =>  {
-    const intl = useIntl()
-    return(
-        <section className="us-header hero is-large " >
-            <div className="hero-body has-text-centered is-flex-mobile is-justify-content-center is-flex-direction-column">
-                <h1 className="is-spaced has-text-dark mb-5" >{intl.formatMessage({ id: data.title })}</h1>
-                <p className="mx-auto subtitle has-text-dark">
-                    {intl.formatMessage({ id: data.subtitle })}
-                </p>
-            </div>
-        </section>
-    )
-}
+const UsHeader = ({ data }) => {
+  const intl = useIntl();
+  return (
+    <section className="us-header hero is-large ">
+      <div className="hero-body has-text-centered is-flex-mobile is-justify-content-center is-flex-direction-column">
+        <h1 className="is-spaced has-text-dark mb-5">
+          {intl.formatMessage({ id: data.title })}
+        </h1>
+        <p className="mx-auto subtitle has-text-dark">
+          {intl.formatMessage({ id: data.subtitle })}
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default UsHeader 
+export default UsHeader;
