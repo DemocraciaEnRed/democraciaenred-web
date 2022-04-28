00bb82a4163db5daf71b31814e04b40dd06e1630
Mariano Valdez <marianovaldez92@protonmail.com>
2022-04-28 15:57:24 -0300

    corrijo sobreescritura del sliderSettings

--- src/components/press-slider/index.js
+++ src/components/press-slider/index.js
@@ -1,36 +1,12 @@
 import React from "react";
-import Slider from "react-slick";
 import { useIntl } from "gatsby-plugin-intl";
 import "./style.scss";
-import sliderSettings from "../slider-settings";
 
 import Press from "../press";
 
 export default ({ slides }) => {
   let boxvalue = false;
   const intl = useIntl();
-  sliderSettings.slidesToShow = 3;
-  sliderSettings.slidesToScroll = 3;
-  sliderSettings.responsive = [
-    {
-      breakpoint: 840,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-  ];
 
    function seeMore  ()  {
     if (boxvalue == false) {
@@ -62,14 +38,9 @@ export default ({ slides }) => {
       <h1 className="pl-6 mb-0 mt-6 our-press title has-text-black">
         {intl.formatMessage({ id: slides.title })}
       </h1>
-      {/* <Slider {...sliderSettings}>
-        {slides.press.map((press) => (
-          <Press data={press} key={press.id} className="pt-6" />
-        ))}
-      </Slider> */}
       <div id="press-box" className=" level press-box ">
-        {slides.press.map((press) => (
-            <Press data={press} key={press.id} className=" pt-6" />
+        {slides.press.map((press, index) => (
+            <Press data={press} key={index} className=" pt-6" />
           ))}
       </div>
       <div className="pos-right">
--- src/components/team-slider/index.js
+++ src/components/team-slider/index.js
@@ -8,35 +8,38 @@ import Team from "../team"
 
 export default ({ slides,title }) =>  {
     const intl = useIntl()
-    sliderSettings.slidesToShow= 3
-    sliderSettings.slidesToScroll= 3
-    sliderSettings.responsive = [
-      {
-        breakpoint: 840,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
+    const sliderSettingsWith3 = {
+      ...sliderSettings,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 840,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true,
+          },
         },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
         },
-      },
-    ];
+      ]
+    }
     
     return (
         <section id="team-slider" className="px-6 team-slider pb-6 hero is-fullheight-deesktop is-large is-primary">
             <h1 className="pl-6 mb-0 mt-6 our-team title has-text-black">{intl.formatMessage({ id: title.slider_title})}</h1>
-            <Slider {...sliderSettings} >
-                {slides.map((team) => 
-                    <Team data={team} key={team.id} className="pt-6"/>
+            <Slider {...sliderSettingsWith3} >
+                {slides.map((team, index) => 
+                    <Team data={team} key={index} className="pt-6"/>
                 )}
             </Slider>
         </section>
