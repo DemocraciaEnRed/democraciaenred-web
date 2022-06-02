7c52b3759453e3cf0ff25baf7a795c26225c3173
DevHormiga <devhormiga@gmail.com>
2022-06-02 12:35:50 -0300

    links de balances correctos

--- src/components/balance-sheets/index.js
+++ src/components/balance-sheets/index.js
@@ -36,25 +36,25 @@ export default ({data}) =>  {
                 <div className="column  balance">
                     <img src={images.violeta}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/1FVbYIdb5saJeqjCqLj08duP1rgPgnLor/view?usp=sharing" className="anio">2021</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2021</a>
                     </div>
                 </div>
                 <div className="column  balance">
                     <img src={images.celeste}></img>
                     <div className="above ">
-                        <a href="https://docs.google.com/document/d/1nPSoBu9U5mO8u2dYsb1JNctdW-qQqWLoq8gdmEjxynQ/edit?usp=sharing" className="anio">2020</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2020</a>
                     </div>
                 </div>
                 <div className="column  balance ">
                     <img src={images.verde}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/1vv4u3m5jfx8_Fe8noNo1vEuin6eZHPl7/view?usp=sharing" className="anio">2019</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2019</a>
                     </div>
                 </div>
                 <div className="column  balance ">
                     <img src={images.amarillo}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/11kp8Iv6PWkjoc3-6JPLfBYQ0xQW7O8WS/view?usp=sharing" className="anio">2018</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2018</a>
                     </div>
                 </div>
             </div>
@@ -62,31 +62,31 @@ export default ({data}) =>  {
                 <div className="column  balance ">
                     <img src={images.naranja}></img>
                     <div className="above" >
-                        <a href="https://drive.google.com/file/d/11kp8Iv6PWkjoc3-6JPLfBYQ0xQW7O8WS/view?usp=sharing" className="anio">2017</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2017</a>
                     </div>
                 </div>
                 <div className="column  balance ">
                     <img src={images.rosa}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/11kp8Iv6PWkjoc3-6JPLfBYQ0xQW7O8WS/view?usp=sharing" className="anio">2016</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2016</a>
                     </div>
                 </div>
                 <div className="column  balance ">
                     <img src={images.violeta}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/1Svu-ozSGfpgS_Rrap5ZqokLXDArxd38l/view?usp=sharing" className="anio">2015</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio">2015</a>
                     </div>
                 </div>
                 <div className="column  balance ">
                     <img src={images.celeste}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/1iGA6FTBmOFx_Xy1mRv6bEjAQYRwfIDC8/view?usp=sharing" className="anio" download>2014</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio" download>2014</a>
                     </div>
                 </div>
                 <div className="column  balance ">
                     <img src={images.verde}></img>
                     <div className="above">
-                        <a href="https://drive.google.com/file/d/1rGABZl_Bw_P5GVSzi6Q3udyNmiOK1SQk/view?usp=sharing" className="anio" download>2013</a>
+                        <a href="https://drive.google.com/drive/folders/1TD-81Yo7G1fdqEZMHex3mRwM-AqTXUWa" className="anio" download>2013</a>
                     </div>
                 </div>
             </div>
