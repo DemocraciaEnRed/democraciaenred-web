8d2e2073fee4b85f4bc79142cb7dafba63caf3b2
DevHormiga <devhormiga@gmail.com>
2022-08-29 16:48:14 -0300

    se modifico el responsive del consejo asesor

--- src/components/council/index.js
+++ src/components/council/index.js
@@ -7,11 +7,11 @@ import Team from "../team"
 const Council =  ({staff}) =>  {
     // const intl = useIntl()
     return (
-        <section id="team-slider" className="px-6 contain-consejo pb-6 hero is-fullheight-deesktop is-large is-primary">
+        <section id="team-slider" className="px-6-desktop contain-consejo pb-6 hero is-fullheight-deesktop is-large is-primary">
             {/* {intl.formatMessage({ id: title.slider_title})} */}
-            <h1 className="pl-6 mb-0 mt-6 our-team title has-text-black">
+            <h1 className="container mb-0 mt-6 our-team title has-text-black">
             Consejo Asesor</h1>
-            <div id="council" className="is-flex is-flex-direction-row is-align-content-center is-flex-wrap-wrap is-flex-grow-5">
+            <div id="council" className="is-flex-desktop is-flex-direction-row is-align-content-center is-flex-wrap-wrap is-flex-grow-5">
                 {staff.map((team, index) => 
                     <Team data={team} key={index} className="pt-6"/>
                 )}
