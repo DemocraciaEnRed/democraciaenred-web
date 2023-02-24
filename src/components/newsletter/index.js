f90fa4d360449037e2cc7b70c2c0886b50ae0aba
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2023-02-24 12:17:04 -0300

    newsletter typeform added

--- src/components/newsletter/index.js
+++ src/components/newsletter/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useIntl, Link } from "gatsby-plugin-intl";
 import "./styles.scss";
 import preview from "./assets/newsletterPreview.png"
+import NewsletterTypeForm from "../newsletter-typeform";
 
 const Newsletter = ({ data }) => {
   const intl = useIntl();
@@ -9,21 +10,16 @@ const Newsletter = ({ data }) => {
   return (
     <section id="newsletter" className="bg-dark py-2">
       <hr className="coloured-divider-left" />
-      <div className="columns mt-0 mx-0 section mb-0 pb-3 pt-4">
-        <div className="column is-one-quarter is-mobile">
-          <img src={preview}></img>
-        </div>
-        <div className="column is-two-thirds">
+      <div className="columns mt-0 mx-0 section mb-0 pb-3 pt-4 newsletter-bg-position" style={{backgroundImage: {preview}}}>
+
+        <div className="column is-offset-one-quarter is-two-thirds">
           <h1 className="has-text-white is-spaced mb-3">
             {intl.formatMessage({ id: data.title })}
           </h1>
           <h2 className="has-text-white is-spaced mb-3">
             {intl.formatMessage({ id: data.description })}
           </h2>
-          <Link className="button button-disabled-shadow has-no-background is-rounded is-medium is-warning is-outlined is-uppercase is-inline-block"
-            to={data.link}>
-            {intl.formatMessage({ id: data.button })}
-          </Link>
+          <NewsletterTypeForm button={intl.formatMessage({ id: data.description })} TFid={data.tfid}/>
         </div>
       </div>
       <hr className="coloured-divider-right" />
