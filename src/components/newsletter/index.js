17250a06491814acb624b1af730c0ce161f452af
GerardoDR <gdrodriguezok@gmail.com>
2023-02-24 10:52:01 -0300

    WIP newsletter

--- src/components/newsletter/index.js
+++ src/components/newsletter/index.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { useIntl } from "gatsby-plugin-intl";
+import { useIntl, Link } from "gatsby-plugin-intl";
 import "./styles.scss";
+import preview from "./assets/newsletterPreview.png"
 
 const Newsletter = ({ data }) => {
   const intl = useIntl();
 
   return (
-    <section id="newsletter">
-      <div className="columns bg-dark mx-0 mt-0 section mb-0 pb-6">
-        <div className="column ">
-          <h1 className="has-text-yellow is-spaced mb-6">
-            {intl.formatMessage({ id: data.subtitle })}
+    <section id="newsletter" className="bg-dark py-2">
+      <hr className="coloured-divider-left" />
+      <div className="columns mt-0 mx-0 section mb-0 pb-3 pt-4">
+        <div className="column is-one-quarter is-mobile">
+          <img src={preview}></img>
+        </div>
+        <div className="column is-two-thirds">
+          <h1 className="has-text-white is-spaced mb-3">
+            {intl.formatMessage({ id: data.title })}
           </h1>
+          <h2 className="has-text-white is-spaced mb-3">
+            {intl.formatMessage({ id: data.description })}
+          </h2>
+          <Link className="button button-disabled-shadow has-no-background is-rounded is-medium is-warning is-outlined is-uppercase is-inline-block"
+            to={data.link}>
+            {intl.formatMessage({ id: data.button })}
+          </Link>
         </div>
       </div>
+      <hr className="coloured-divider-right" />
     </section>
   );
 };
 
-export default ResourcesHeader;
+export default Newsletter;
