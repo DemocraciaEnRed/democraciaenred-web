e90ce03f88fb488f4e83316b6f7df264b6ded696
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2023-04-12 17:37:48 -0300

    small tweaks

--- src/components/blog-content/index.js
+++ src/components/blog-content/index.js
@@ -64,46 +64,48 @@ const BlogContent = ({ post }) => {
           </div>
         </div>
         <div className="section is-small">
-          <div className="columns is-mobile post-columns">
-            <div className="column is-three-quarters-tablet is-four-fifths-desktop">
-              <h1 className="pb-4">{post.content.title}</h1>
-              {/* <h2 className="pb-4 has-text-black">{post.content.description}</h2> pendiente de consulta sobre su integración */}
-              {post.content.bodymd ? (
-                <article
-                  className="post-body"
-                  dangerouslySetInnerHTML={{
-                    __html: mdTransformer(post.content.bodymd),
-                  }}
-                  style={{ color: "#000" }}
-                ></article>
-              ) : (
-                "Contenido en desarrollo..."
-              )}
-            </div>
-            <div className="column is-one-third-tablet is-one-fifth-desktop">
-              <div className="columns is-mobile is-multiline has-text-right post-info-column">
-                <div className="column is-narrow-mobile is-full-tablet">
-                  <b className="is-block">Autor</b>
-                  <p>{findAuthor()}</p>
-                </div>
-                <div className="column is-narrow-mobile is-full-tablet">
-                  <b className="is-block">Publicado</b>
-                  {dateParse(post.published_at)}
-                </div>
-                {post.tag_list.length > 0 ?
-                  (<div className="column is-full-tablet">
-                    <b className="is-block">Etiquetas</b>
-                    <div className="is-flex is-flex-wrap-wrap tags-wrapper">
-                    {post.tag_list.map((tag, index) => (
-                      <span
-                        key={index}
-                        className="tag is-warning is-rounded has-text-black my-1"
-                      >
-                        {tag}
-                      </span>))}
+          <div className="container">
+            <div className="columns is-mobile post-columns">
+              <div className="column is-three-quarters-tablet is-four-fifths-desktop">
+                <h1 className="pb-4">{post.content.title}</h1>
+                <h2 className="pb-4 has-text-black">{post.content.description}</h2>
+                {post.content.bodymd ? (
+                  <article
+                    className="post-body"
+                    dangerouslySetInnerHTML={{
+                      __html: mdTransformer(post.content.bodymd),
+                    }}
+                    style={{ color: "#000" }}
+                  ></article>
+                ) : (
+                  "Contenido en desarrollo..."
+                )}
+              </div>
+              <div className="column is-one-third-tablet is-one-fifth-desktop">
+                <div className="columns is-mobile is-multiline has-text-right post-info-column">
+                  <div className="column is-narrow-mobile is-full-tablet">
+                    <b className="is-block">Autor</b>
+                    <p>{findAuthor()}</p>
+                  </div>
+                  <div className="column is-narrow-mobile is-full-tablet">
+                    <b className="is-block">Publicado</b>
+                    {dateParse(post.published_at)}
+                  </div>
+                  {post.tag_list.length > 0 ?
+                    (<div className="column is-full-tablet">
+                      <b className="is-block">Etiquetas</b>
+                      <div className="is-flex is-flex-wrap-wrap tags-wrapper">
+                        {post.tag_list.map((tag, index) => (
+                          <span
+                            key={index}
+                            className="tag is-warning is-rounded has-text-black my-1"
+                          >
+                            {tag}
+                          </span>))}
                       </div>
-                  </div>) :
-                  ""}
+                    </div>) :
+                    ""}
+                </div>
               </div>
             </div>
           </div>
