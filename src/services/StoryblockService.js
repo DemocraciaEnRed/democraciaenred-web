2e1d69ac53a24d18b299b0dbb6d4f06c77d032f6
Guillermo Croppi <guillermocroppi@gmail.com>
2023-04-19 13:08:40 -0300

    Cheat for build process to detect if Headers() API is available (which, is not)

--- src/services/StoryblockService.js
+++ src/services/StoryblockService.js
@@ -1,14 +1,22 @@
 import StoryblokClient from "storyblok-js-client";
 
-export const StoryblockService = () => {
+const isBrowser = typeof Headers !== "undefined"
 
-    if (typeof StoryblokInstance !== "object") {
+export const StoryblockService = () => {
+    // check if class Headers() from the Web API is available
+    if(isBrowser) {
         const StoryblokInstance = new StoryblokClient({
             accessToken: process.env.STORYBLOK_TOKEN,
             region: process.env.STORYBLOK_REGION
         });
         return StoryblokInstance;
     } else {
-        return StoryblokInstance;
+        return null;
     }
+
+    
+    // if (typeof StoryblokInstance !== "object") {
+    // } else {
+    //     return StoryblokInstance;
+    // }
 }
